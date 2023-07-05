592f359f20ed6e3cbe674f3f9706cee7fa519d2a
ncaldwell17 <ncaldwellgatsos@gmail.com>
2023-07-05 14:21:36 -0500

    Added login animation

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,12 +1,49 @@
 // General
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-const Login: React.FC = () => {
+const LoginPage: React.FC = () => {
+    const [visible, setVisible] = useState(false);
+    const [large, setLarge] = useState(false);
+    const [showBox, setShowBox] = useState(false);
+
+    useEffect(() => {
+        const timerVisible = setTimeout(() => {
+            setVisible(true);
+            const timerScale = setTimeout(() => {
+                setLarge(true);
+                setTimeout(() => {
+                    setShowBox(true);
+                }, 3000); // show the box 0.5s after the image enlarges
+            }, 1000); // start scaling 1s after the image becomes visible
+
+            return () => clearTimeout(timerScale);
+        }, 1000); // start fading in 1s after the component mounts
+
+        return () => clearTimeout(timerVisible);
+    }, []);
+
     return (
-        <div className="flex items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-4xl text-blue-500 font-bold">Login Page</h1>
+        <div
+            className="flex justify-center items-center h-screen"
+        >
+            <img
+                src="ink_black.svg"
+                alt="Ink Black"
+                className="w-1/2 h-1/2 absolute"
+                style={{
+                    transform: large
+                        ? 'scale(5) translateX(-2%)'
+                        : 'scale(0.1) translateX(0)',
+                    transition: 'transform 2.5s ease-in-out, opacity 0.25s ease-in-out',
+                    opacity: visible ? 1 : 0,
+                    zIndex: 1
+                }}
+            />
+            {showBox && (
+                <div className="w-80 h-96 border-2 border-white absolute z-10"></div>
+            )}
         </div>
     );
 }
 
-export default Login;
+export default LoginPage;
